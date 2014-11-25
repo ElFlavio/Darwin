2be77aaa7192d9d5febc23665936cb4f7e07c1e5
Aubin Gerard <aubin.gerard@gmail.com>
2014-11-25 15:44:57 +0100

    Update Server config adding couch-node module config

--- server/server.js
+++ server/server.js
@@ -1,7 +1,17 @@
 var http = require('http');
+var CouchDB = require('couch-db').CouchDB;
+    server = new CouchDB('http://localhost:5984'); // instance de couch-db
 
-httpServer = http.createServer(function(req, res){
+// demare le server et ecoute sur la racine
+httpServer = http.createServer(function(req, res) {
+	res.writeHead(200, {'Content-Type': 'text/plain'});;
+	var db = server.database('user');
+	// execute select query in couch db
+	db.select().limit(1).exec(function(err, res){
+		console.log(err);
+		console.log(res);
+	});
+	
+	// display result in browser
 	res.end('hello World');
-});
-
-httpServer.listen(8080);
+}).listen(8080);// port d'ecoute
