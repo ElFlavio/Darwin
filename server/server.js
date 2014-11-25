58574bcd4d11c895e82291bf125a96e781b89404
Aubin Gerard <aubin.gerard@gmail.com>
2014-11-25 22:20:00 +0100

    Adding Ejs for templating, update server.js to work with ejs, adding new template file ejs

--- server/server.js
+++ server/server.js
@@ -1,14 +1,25 @@
 var express = require('express');
 var app = express();
+var path = require('path');
+http = require('http');
+server = http.createServer(app);
 
+// templating configuration
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/dist'));
+
+// routing
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.render('index', { title: 'The index page!' });
 });
 
-app.get('/get', function (req, res) {
-  res.sendfile('index.html'); // load html
+/* => @TODO choose between angular pasoting template or send data to template with ejs
+app.get('/:page', function (req, res) {
+	page = req.params.page;
+  res.render(page, { title: 'Hey', message: 'Hello there!'});
 });
-
+*/
+// server configuration
 var server = app.listen(3000, function () {
 
   var host = server.address().address;
